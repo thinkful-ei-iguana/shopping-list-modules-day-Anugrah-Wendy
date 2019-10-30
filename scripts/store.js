14fbaceff5ddab1b1ea8ee3a8a607973fb939809
Anugrah Lambogo <lambogoai@gmail.com>
2019-10-30 14:22:33 -0500

    finished number 6

--- scripts/store.js
+++ scripts/store.js
@@ -1,12 +1,53 @@
 import item from './item.js';
 
-
-
 let items = [];
 let hideCheckedItems = 'false';
 
+function findById(id) {
+  return items.find(a => a.id === id);
+}
+
+function addItem(name) {
+  try{
+    item.validateName(name);
+    let newItem = item.create(name);
+    this.items.push(newItem);
+  }
+  catch(e){
+    console.log(`name can not be blank ${e.message}`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  let target = this.findById(id);
+  target.checked = !target.checked;
+  console.log(target);
+}
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(newName);
+    let updatedItem = this.findById(id);
+    updatedItem.name = newName;
+  }
+  catch(e) {
+    console.log(`Cannot update name: ${e.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  const ind = this.items.findIndex(a => a.id === id);
+  this.items.splice(ind, 1);
+}
+
+
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
 };
 
